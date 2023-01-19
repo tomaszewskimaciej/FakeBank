cd17af0d20a25b5790a57a190d9b0d4a59cb9da1
Tomaszewski Maciej <tomaszewski.maciej67@gmail.com>
2023-01-19 16:06:21 +0100

    Implement multi yaml factory

--- FakeBank/app/src/main/java/com/fake/bank/backend/app/Application.java
+++ FakeBank/app/src/main/java/com/fake/bank/backend/app/Application.java
@@ -1,18 +1,29 @@
 package com.fake.bank.backend.app;
 
+import com.fake.bank.backend.app.config.YamlPropertySourceFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({"com.fake.bank"})
 @EntityScan({"com.fake.bank.backend.integration.entity"})
 @EnableJpaRepositories("com.fake.bank.backend.integration.entity")
+@PropertySources({
+        @PropertySource(value = "classpath:integration.yaml", factory = YamlPropertySourceFactory.class),
+        @PropertySource(value = "classpath:rest.yaml", factory = YamlPropertySourceFactory.class)
+})
 public class Application {
 
+    private static final String SPRING_CONFIG_NAME = "spring.config.name";
+    private static final String APPLICATION_NAME = "fakeBank";
+
     public static void main(String[] args) {
+        System.setProperty(SPRING_CONFIG_NAME, APPLICATION_NAME);
         SpringApplication.run(Application.class, args);
     }
 }
